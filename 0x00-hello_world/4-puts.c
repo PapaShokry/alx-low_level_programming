f8ffd69f88059bd3efcdd56e8c2667445f95d3b4
PapaShokry <islamshokry7@gmail.com>
2023-02-16 17:52:35 -0800

    Add C file for printing text 4. task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ * Prints a text in stdout
+ */
+int main(void)
+{
+char myStr[] = "\"Programming is like building a multilingual puzzle\n";
+puts(mystr);
+return (0);
+}
