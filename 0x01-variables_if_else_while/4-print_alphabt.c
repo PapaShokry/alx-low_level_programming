f2f2c14b7dfb3b88739152eed41a68437762e04c
PapaShokry <islamshokry7@gmail.com>
2023-02-17 17:11:46 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+	        if(ch != 'e' && ch != 'q')
+                {
+                        putchar(ch);
+                }
 	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar(ch);
-	}
+        putchar('\n')
 	return (0);
 }
 
