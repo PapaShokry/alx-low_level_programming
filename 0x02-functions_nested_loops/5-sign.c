e49c487fce6635179d0627f850078b667d3187f2
PapaShokry <islamshokry7@gmail.com>
2023-02-22 11:18:48 -0800

    Fix code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_sign - entry to function
- * @c: integer recived as number
- * Descrption: prints a sign of number next to it
- * Return: string 0
+ * print_sign - Entr point
+ * @n: carrier variable
+ * Description: prints the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
  */
 
-void print_sign(int c)
+int print_sign(int n)
 {
-if (c > 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (c < 0)
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
