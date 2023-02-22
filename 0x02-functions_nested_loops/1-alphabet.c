a42b94e5aca349eb5685fe251511ca72c0cf2b73
PapaShokry <islamshokry7@gmail.com>
2023-02-22 08:39:41 -0800

    Update change code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - entry point for program
- * Description: call a function the prints all alphabetics in lower case
- * Return: always 0
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
