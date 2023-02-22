a7d71c9fcce987bec5de6cbbddd5f9aa3f0f28c2
PapaShokry <islamshokry7@gmail.com>
2023-02-22 09:16:35 -0800

    add C file for printing alpha 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * print_alphabet_x10 - main function to print alpha 10 times
+ *
+ *
+ * Return: always 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+char alpha = 'a'
+for (int i = 0; i< 10 ; i++)
+{
+while (alpha <='z')
+{
+_putchar(alpha);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/shymonty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n = 100;
+for(int i = 0; i < n; i++)
+{
+for (int j = i + 1; j > 0 ; j--)
+{
+printf("%s",  "بحبك,");
+
+}
+printf("%c", '\n');
+}
+}
