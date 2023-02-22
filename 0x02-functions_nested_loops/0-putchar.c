11240c3e46e8c73609842d54b4247376b85c9439
PapaShokry <islamshokry7@gmail.com>
2023-02-22 05:40:34 -0800

    Add program that writes _putchar 0. task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to the program
+ * Description: prints a word into output
+ * Return:0
+ */
+
+int main(void)
+{
+printf("%s\n", "_putcahr");
+return (0);
+}
