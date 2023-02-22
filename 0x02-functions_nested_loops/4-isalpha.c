df48ea9fa96266a1525b626899af8699054138b3
PapaShokry <islamshokry7@gmail.com>
2023-02-22 10:55:51 -0800

    Fix decalartion

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,19 +2,21 @@
 
 /**
  * _isalpha - checks if character is alpha
- * Description: a function that accepts int of characters and checks if it is alpha characters
+ * Description: a function that accepts int of char and checks if it is alpha char
+ * @c; the integer value it receives
  * Return: 1 if true and 0 if false
  */
 
 int _isalpha(int c)
 {
-int i;
+        int i;
         for (i = 65; i <= 122; i++)
         {
                 if (i == c)
-                {
+                {       
                         return (1);
                 }
         }
+        
         return (0);
 }
