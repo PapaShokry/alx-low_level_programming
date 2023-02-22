f6fb2e2af23238fdcbc1fdc749a9e059f55ab907
PapaShokry <islamshokry7@gmail.com>
2023-02-22 10:45:30 -0800

    Add C file that checks if certain char is alpha 4. task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is alpha
+ * Description: a function that accepts int of characters and checks if it is alpha characters
+ * Return: 1 if true and 0 if false
+ */
+
+int _isalpha(int c)
+{
+        for (int i = 65; i <= 122, i++)
+        {
+                if (i == c)
+                {
+                        return (1);
+                }
+        }
+        return (0);
+}
