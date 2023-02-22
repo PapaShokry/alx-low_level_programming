e5575b3d51eb618e67cf3058398de2e244258250
PapaShokry <islamshokry7@gmail.com>
2023-02-22 08:33:51 -0800

    Add file to print lower case alpha [not sure if it will work]

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point for program
+ * Description: call a function the prints all alphabetics in lower case
+ * Return: always 0
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
