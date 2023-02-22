ede9824368d9a0cd2aed763e9dee14ad19170e08
PapaShokry <islamshokry7@gmail.com>
2023-02-22 11:32:19 -0800

    Add c file that returns the absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - entry to function
+ * @n: the integer received
+ * Description: Returns the absolute value of an integer
+ * Return: 0
+ */
+int _abs(int n)
+{
+return (n < 0 ? -n : n);
+}
