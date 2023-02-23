f4b279c813ec1aa77696b4ba2125aa053c0935bb
PapaShokry <islamshokry7@gmail.com>
2023-02-23 12:54:33 -0800

    Add main.c and main.h for 0.task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* main - entry to program
-* Description: tests func that prints if integer is + or -
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 98;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
