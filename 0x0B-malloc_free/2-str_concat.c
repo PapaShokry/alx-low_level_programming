6150b56b4c6eb08b181bf0d40c0b0a37fc2ae4cf
PapaShokry <islamshokry7@gmail.com>
2023-07-11 18:05:34 +0200

    Add 3rd task string concatination

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,28 +11,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    char *s3;
-    int i, j, k, l;
+	char *s3;
+	int i, j, k, l;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (i = 0 ; s1[i] != '\0' ; i++);
-    for (j = 0 ; s2[j] != '\0' ; j++);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-    s3 = malloc(sizeof(char) * (i + j + 1));
+	s3 = malloc(sizeof(char) * (i + j + 1));
 
-    if (s3 == NULL)
-        return (NULL);
+	if (s3 == NULL)
+		return (NULL);
 
-    for (k = 0 ; k < i ; k++)
-        s3[k] = s1[k];
-    for (l = 0 ; l < j ; l++)
-        s3[k + l] = s2[l];
+	for (k = 0; k < i; k++)
+		s3[k] = s1[k];
+	for (l = 0; l < j; l++)
+		s3[k + l] = s2[l];
 
-    s3[k + l] = '\0';
+	s3[k + l] = '\0';
 
-    return (s3);
+	return (s3);
 }
