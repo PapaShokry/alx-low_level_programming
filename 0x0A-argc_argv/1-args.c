75912edeca786ebae7c104a12580084fe9295e68
PapaShokry <islamshokry7@gmail.com>
2023-07-16 20:28:40 +0300

    Add 2nd  task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,10 @@
  * main - main function to print number of passed arguments
  * @argc: number of arguments
  * @argv: arguments
- * Returns: number of passed arguments
+ * Return: 0 - success
  */
 
-int main(int argc, char __attribute__((unused)) * argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
