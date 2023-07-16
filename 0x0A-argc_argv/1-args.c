2bf3d09922ad88497108cb65f087be1d56aadc06
PapaShokry <islamshokry7@gmail.com>
2023-07-16 20:23:51 +0300

    Add 2nd  task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - main function to print number of passed arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ * Returns: number of passed arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
