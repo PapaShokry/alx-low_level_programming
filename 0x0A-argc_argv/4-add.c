57bff285cb9859357b6d07d46e827d78ed6a5b38
PapaShokry <islamshokry7@gmail.com>
2023-07-16 20:53:28 +0300

    last mandatory task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - function that adds positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 - succes or 1 on error
+ * main - adds positive numbers
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int i, j, sum = 0;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i]))
-			sum += atoi(argv[i]);
-		else
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+	return (0);
 }
